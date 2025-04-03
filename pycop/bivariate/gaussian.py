5fcff638a4eb4dabbf6266d4383f5b8307958517
Odin Gramstad <odin.gramstad@dnv.com>
2025-04-03 12:22:43 +0200

    Adding derivative of cdf for gaussian bivariate copula

--- pycop/bivariate/gaussian.py
+++ pycop/bivariate/gaussian.py
@@ -16,7 +16,7 @@ class gaussian(copula):
     bounds_param : list
         A list that contains the domain of the parameter(s) in a tuple.
         Exemple : [(lower, upper)]
-    parameters_start : array 
+    parameters_start : array
         Value(s) of the initial guess when estimating the copula parameter(s).
         It represents the parameter `x0` in the `scipy.optimize.minimize` function.
 
@@ -33,7 +33,7 @@ class gaussian(copula):
         super().__init__()
         self.family = "gaussian"
         self.bounds_param = [(-1, 1)]
-        self.parameters_start = np.array(0) 
+        self.parameters_start = np.array(0)
 
     def get_cdf(self, u, v, param):
         """
@@ -42,7 +42,7 @@ class gaussian(copula):
         Parameters
         ----------
         u, v : float
-            Values of the marginal CDFs 
+            Values of the marginal CDFs
         param : list
             The correlation coefficient param[0] ∈ [-1,1].
             Used to defined the correlation matrix (squared, symetric and definite positive)
@@ -54,6 +54,38 @@ class gaussian(copula):
 
         return multivariate_normal.cdf(np.array([y1, y2]).T, mean=None, cov=[[1, rho], [rho, 1]])
 
+    def get_dcdf_duv(self, deriv, u, v, param):
+        """
+        # Computes the partial derivative of the CDF
+
+        Parameters
+        ----------
+        deriv : string
+             Which derivative to take: 'u' or 'v'.
+        u, v : float
+            Values of the marginal CDFs
+        param : list
+            The correlation coefficient param[0] ∈ [-1,1].
+            Used to defined the correlation matrix (squared, symetric and definite positive)
+        """
+        y1 = norm.ppf(u, 0, 1)
+        y2 = norm.ppf(v, 0, 1)
+
+        pdf_y1 = norm.pdf(y1)
+        pdf_y2 = norm.pdf(y2)
+
+        dy1_du = 1/pdf_y1
+        dy2_dv = 1/pdf_y2
+
+        rho = param[0]
+
+        if deriv == 'u':
+            yt = (y2 - rho*y1)/np.sqrt(1 - rho**2)
+            return dy1_du*norm.pdf(y1)*norm.cdf(yt)
+        elif deriv == 'v':
+            yt = (y1 - rho*y2)/np.sqrt(1 - rho**2)
+            return dy2_dv*norm.pdf(y2)*norm.cdf(yt)
+
     def get_pdf(self, u, v, param):
         """
         # Computes the PDF
@@ -61,17 +93,15 @@ class gaussian(copula):
         Parameters
         ----------
         u, v : float
-            Values of the marginal CDFs 
+            Values of the marginal CDFs
         param : list
             The correlation coefficient param[0] ∈ [-1,1].
             Used to defined the correlation matrix (squared, symetric and definite positive)
         """
-        
+
         rho = param[0]
         a = np.sqrt(2) * erfinv(2 * u - 1)
         b = np.sqrt(2) * erfinv(2 * v - 1)
         det_rho = 1 - rho**2
-        
+
         return det_rho**-0.5 * np.exp(-((a**2 + b**2) * rho**2 -2 * a * b * rho) / (2 * det_rho))
-
-
